5adeb8e10e2464ba2d057dd8487784323b540a02
Guilherme Alan Ritter <gui.a.ritter@gmail.com>
2024-07-10 15:50:38 -0300

    added STOMP init

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
-import 'dotenv/config'; // always first import
-import express from 'express';
 import 'babel-polyfill';
 import cors from 'cors';
+import 'dotenv/config'; // always first import
+import express from 'express';
 
 import logRoute from './app/route/logRoute';
+import { getLog } from './app/util/log';
+
+const log = getLog('index');
+
+// TODO I think that this is what I need: https://github.com/pcan/node-stomp-protocol
+var stomp = require('stomp');
 
 const app = express();
 
@@ -14,6 +20,49 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.use('/', logRoute);
+
+// STOMP
+
+// Set to true if you want a receipt
+// of all messages sent.
+const receipt = true;
+
+// Set debug to true for more verbose output.
+// login and passcode are optional (required by rabbitMQ)
+const stomp_args = {
+    port: 51091,
+    // host: 'guilherme-alan-ritter.net',
+    host: 'localhost',
+    debug: true,
+    // login: 'guest',
+    // passcode: 'guest',
+}
+
+const client = new stomp.Stomp(stomp_args);
+
+client.connect();
+
+client.on('connected', () => {
+	log('client connected');
+	client.disconnect();
+});
+
+client.on('receipt', receipt => {
+	log('receipt received');
+});
+
+client.on('error', function(error_frame) {
+    console.log(error_frame.body);
+    client.disconnect();
+});
+
+process.on('SIGINT', () => {
+	log('received SIGINT');
+	client.disconnect();
+	process.exit(0);
+});
+
+// back to Express
 
 app.listen(process.env.PORT, '127.0.0.1').on('listening', () => {
 	console.log(`${(new Date()).toISOString()} are live on ${process.env.PORT}`);
